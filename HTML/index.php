10a81691af751efb68fec9f211b7b6ae87a275c8
Tjoosten <Tjoosten3@gmail.com>
2015-01-14 22:05:15 +0100

    Add html templates

---
+++ HTML/index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+
+    <!-- Meta -->
+
+    <!-- Stylesheet -->
+
+    <title> Starter Template </title>
+  </head>
+  <body>
+
+    <!-- JavaScript -->
+    <!-- Placed in bottom because the page loads faster then -->
+  </body>
+</html>
